dcdb3fd1aeab0209348fda7964909ab97dceb912
Ridhima Binolkar <binolkarridhima@gmail.com>
2017-08-18 20:31:40 -0500

    Add files via upload

---
+++ Bankers.java
@@ -0,0 +1,103 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Bankers{
+    private int need[][],allocate[][],max[][],avail[][],np,nr;
+ 	static PrintStream outStream ;
+    private void input(String fileName)  {
+    	
+    	try{
+    	Scanner sc=new Scanner(new File(fileName));
+    	
+    	
+    	//System.out.print("Enter no. of processes and resources : ");
+    	np=sc.nextInt();  //no. of process
+    	nr=sc.nextInt();  //no. of resources
+    	need=new int[np][nr];  //initializing arrays
+    	max=new int[np][nr];
+    	allocate=new int[np][nr];
+    	avail=new int[1][nr];
+    	
+    
+    	for(int i=0;i<np;i++)
+          for(int j=0;j<nr;j++)
+    	    allocate[i][j]=sc.nextInt();  //allocation matrix
+    		
+    
+    	for(int i=0;i<np;i++)
+          for(int j=0;j<nr;j++)
+    	    max[i][j]=sc.nextInt();  //max matrix
+    		
+       
+        for(int j=0;j<nr;j++)
+    	    avail[0][j]=sc.nextInt();  //available matrix
+        
+        sc.close();
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+    }
+    
+    private int[][] calc_need(){
+       for(int i=0;i<np;i++)
+         for(int j=0;j<nr;j++)  //calculating need matrix
+	         need[i][j]=max[i][j]-allocate[i][j];
+       
+       return need;
+    }
+	
+    private boolean check(int i){
+       //checking if all resources for ith process can be allocated
+       for(int j=0;j<nr;j++)
+	      if(avail[0][j]<need[i][j])
+	         return false;
+		 
+	   return true;
+    }
+
+    public void isSafe(String fileName) {
+       input(fileName);
+       calc_need();
+       boolean done[]=new boolean[np];
+       int j=0;
+
+       while(j<np){  //until all process allocated
+	      boolean allocated=false;
+    	  for(int i=0;i<np;i++)
+     		 if(!done[i] && check(i)){  //trying to allocate
+	           for(int k=0;k<nr;k++)
+		          avail[0][k]=avail[0][k]-need[i][k]+max[i][k];
+	           writeLn("Allocated process : "+i);
+		         allocated=done[i]=true;
+               j++;
+             }
+          if(!allocated) break;  //if no allocation
+	   }
+       if(j==np)  //if all processes are allocated
+	       writeLn("Safely allocated");
+       else
+    	   writeLn("All proceess cant be allocated safely");
+    	   
+    }
+    
+    public static void main(String[] args) {
+    	
+    	String inputFileName="D:/Safe.txt";
+    	String logFileName="D:/Bankerslog.log";
+    	try{
+    	  outStream = new PrintStream(new FileOutputStream(logFileName));
+		new Bankers().isSafe(inputFileName);
+    	}catch(Exception e){
+    		
+    	}
+	}
+    
+	public void writeLn(String msg){
+		
+		if(outStream!=null){
+			outStream.println(msg);
+		}
+	}
+}
